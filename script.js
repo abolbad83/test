9f5b18cbcef9504f95f6c0a555f915fe169b8b7e
abolipa <jkhan.badri.80@gmail.com>
2024-05-03 10:23:06 +0330

    commited

---
+++ script.js
@@ -0,0 +1,31 @@
+// let sum = {id:1 ,Name:'ali'}
+//   // {id:2 ,Name:'parsa'},
+//   // {id:3 ,Name:'abol'}
+
+
+
+// let god = {id , Name} = sum
+
+// console.log(id,Name);
+///////////////////////////////////////////////////////////
+const vara = (a,b,c,...args) => {
+  
+  let nums = [a,b,c,...args]
+   
+    return nums
+}
+ console.log(vara(1,2,3,4,55,67,8,9))
+
+ const albc = (a,b) => {
+   let num = a+b
+    return num
+ }
+
+ console.log(albc(1,2));
+
+ ///////////////////////////////////////////////////////
+
+ let akrab = 'akrabb'
+ let arab = `ali is ${10} years old and 2+2 is ${2+2} or ${akrab}`
+
+ console.log(arab);
